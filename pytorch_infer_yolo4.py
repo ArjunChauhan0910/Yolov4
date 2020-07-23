662bcaa77f5ace703f4107beaab9264a7196c8b6
Arjun Chauhan <arjunchau@gmail.com>
2020-07-23 09:55:17 +0530

    Updated Inferencing
    Video Stream Integrated

--- pytorch_infer_yolo4.py
+++ pytorch_infer_yolo4.py
@@ -5,6 +5,7 @@ import glob
 from trident import *
 from pytorch_yolo import *
 from pytorch_darknet import *
+import cv2
 
 
 detector=YoloDetectionModel(input_shape=(3,608,608),output=yolo4_body(80, 608))
@@ -46,18 +47,18 @@ with open('pretrained/modified_classes.txt', 'r', encoding='utf-8-sig') as f:
 detector.palette = generate_palette(80)
 
 
-imgs=glob.glob('images/*.*g')
-imgs=[img for img in imgs if   '_infered' not in  img]
-print('total {0} images'.format(len(imgs)))
 
 is_drawing = True
-for i in range(len(imgs)):
-    folder, filename, ext = split_path(imgs[i])
-    if is_drawing:
-        img = detector.infer_then_draw_single_image(imgs[i], verbose=True)
-        if is_small_item_enhance:
-            array2image(img).save(imgs[i].replace(filename, filename + '_pt_infered_enhance'))
-        else:
-            array2image(img).save(imgs[i].replace(filename, filename + '_pt_infered'))
-    else:
-        detector.infer_single_image(imgs[i], verbose=True)
+cam = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    img = detector.infer_then_draw_single_image(frame, verbose=True)
+    img =  array2image(img)
+    cv2.imshow('Detected',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+      
+cap.release()
+cv2.destroyAllWindows()        
